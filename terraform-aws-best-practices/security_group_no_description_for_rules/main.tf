f5093a90fc409ae13005154e82fed586c7b1938a
hugh-whitesource <77014894+hugh-whitesource@users.noreply.github.com>
2021-05-25 11:11:37 -0400

    Update main.tf

--- terraform-aws-best-practices/security_group_no_description_for_rules/main.tf
+++ terraform-aws-best-practices/security_group_no_description_for_rules/main.tf
@@ -21,6 +21,7 @@ resource "aws_security_group" "default" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/24"]
+    description = "ingress"
   }
 
   egress {
@@ -28,6 +29,7 @@ resource "aws_security_group" "default" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "egress"
   }
 
   tags = {
